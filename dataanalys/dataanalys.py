d6eb7bfb3f4e499b04901ea15bd7fe8475c41555
R2StN <bengus04@gmail.com>
2026-03-27 23:33:12 +0100

    Analys av vätespektra

--- dataanalys/dataanalys.py
+++ dataanalys/dataanalys.py
@@ -3,17 +3,90 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 
-file = pd.read_csv('dataanalys/mätningar/mätning', sep=';', delimiter=',')
+file_Na = pd.read_csv('dataanalys/mätningar/Natrium_FULL_SPEKTRA.csv', sep=';')
+file_H1 = pd.read_csv('dataanalys/mätningar/Väte_mätning_1.csv', sep = ';')
 
-wavelenghts = np.array(file['Wavelenght'])
-intensities = np.array(file['Intensity'])
+files = [file_Na, file_H1]
 
-peaks = find_peaks(intensities)
-intensity_peaks = intensities[peaks]
+R = 1.097e-2
 
-fig, ax = plt.subplots()
+def spectra(file, plot=True):
+    file['Intensity'] = file['Intensity'].str.replace(',', '.').astype(float)
+    file['Wavelenght'] = file['Wavelenght'].str.replace(',', '.').astype(float)
 
-plt.plot(wavelenghts, intensities)
-plt.scatter(wavelenghts, intensity_peaks, color = 'r')
+    wavelenghts = np.array(file['Wavelenght'].values)
+    intensities = np.array(file['Intensity'].values)
 
-plt.show()
+    peaks = find_peaks(intensities, height=0.01, prominence=0.005*np.max(intensities))[0]
+    intensity_peaks = intensities[peaks]
+    wavelenghts_peaks = wavelenghts[peaks]
+
+    fig, ax = plt.subplots()
+
+    plt.plot(wavelenghts, intensities)
+    plt.scatter(wavelenghts_peaks, intensity_peaks, color = 'r')
+    if plot == True:
+        plt.show()
+
+    return np.sort(wavelenghts_peaks)
+
+Na_peaks = spectra(files[0])
+
+H_peaks_teo = []
+    
+for n1 in range(1, 4):
+    for n2 in range(n1+1, 15):
+        inv_lambda = R * (1/n1**2 - 1/n2**2)
+            
+        if inv_lambda > 0:
+            lam = 1 / inv_lambda
+            if 200<lam<1100:    
+                H_peaks_teo.append({
+                    "n1": n1,
+                    "n2": n2,
+                    "lambda": lam
+                    })
+
+# for i in H_peaks_teo:
+#     print(i['lambda'])
+
+def match_lines(measured, transitions, tol=0.005):
+    matches = []
+    
+    for lam_meas in measured:
+        best = None
+        best_error = np.inf
+        
+        for t in transitions:
+            lam_theory = t["lambda"]
+            
+            error = abs(lam_meas - lam_theory) / lam_theory
+            
+            if error < best_error:
+                best_error = error
+                best = t
+        
+        if best_error < tol:
+            matches.append({
+                "measured_nm": lam_meas,
+                "n1": best["n1"],
+                "n2": best["n2"],
+                "theory_nm": best["lambda"],
+                "rel_error": best_error
+            })
+        else:
+            matches.append({
+                "measured_nm": lam_meas,
+                "n1": None,
+                "n2": None,
+                "theory_nm": None,
+                "rel_error": None
+            })
+    
+    return pd.DataFrame(matches)
+
+
+H_peaks = spectra(files[1])
+what = match_lines(H_peaks, H_peaks_teo)
+
+print(what)
