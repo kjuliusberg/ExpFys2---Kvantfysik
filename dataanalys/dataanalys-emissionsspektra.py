de253c04af751fa12b6ab2e43c26c1ea397a019d
R2StN <bengus04@gmail.com>
2026-04-04 11:38:23 +0200

    Skrev klart resultat och la in kod
    Basically klar med metod + resultat. Ska antagligen finslipas lite när teorin är klar. Kanske också lägga till lite om de relativa intensiteterna.

--- dataanalys/dataanalys-emissionsspektra.py
+++ dataanalys/dataanalys-emissionsspektra.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
+
+
+
+# importera alla filer
 file_NA = pd.read_csv('dataanalys/mätningar/Natrium_Full_Spectra_v2.csv', sep=';')
 file_H1 = pd.read_csv('dataanalys/mätningar/Väte_mätning_1.csv', sep=';')
 file_H2 = pd.read_csv('dataanalys/mätningar/Väte_mätning_2.csv', sep=';')
@@ -9,24 +13,26 @@ file_H3 = pd.read_csv('dataanalys/mätningar/Väte_mätning_3.csv', sep=';')
 
 files = [file_NA, file_H1, file_H2, file_H3]
 
-# Fix decimal format
+# Fixa decimal format
 for file in files:
     file['Intensity'] = file['Intensity'].str.replace(',', '.').astype(float)
     file['Wavelenght'] = file['Wavelenght'].str.replace(',', '.').astype(float)
 
 # NATRIUM
-
+# Variabler för att hitta toppar
 prominence_NA = 0.008
 distance_NA = 10
 
 wavelengths_NA = file_NA['Wavelenght'].values
 intensities_NA = file_NA['Intensity'].values
 
+# Hitta toppar
 peaks_NA, props = find_peaks(intensities_NA, prominence=prominence_NA, distance=distance_NA)
 
 H1_wave_peaks = wavelengths_NA[peaks_NA]
 H1_ints_peaks = intensities_NA[peaks_NA]
 
+# Plotta spektra med toppar markerade
 plt.figure()
 plt.plot(wavelengths_NA, intensities_NA)
 plt.scatter(wavelengths_NA[peaks_NA], intensities_NA[peaks_NA], color='red')
@@ -35,6 +41,7 @@ plt.ylabel('Intensitet [arb.]')
 plt.title('Emissionsspektra-Natrium')
 #plt.savefig('NA_spectra.png')
 
+# Zooma in på dubbeltopp
 na_min = np.where(wavelengths_NA.astype(int) == 585)[0][0]
 na_max = np.where(wavelengths_NA.astype(int) == 595)[0][0]
 
@@ -46,19 +53,20 @@ plt.ylabel('Intensitet [arb.]')
 plt.title('Emissionsspektra-Natrium')
 #plt.savefig('NA_spectra_doublepeaks.png')
 
-
+# VÄTE - FIL 1
 prominence_H1 = 0.008
 distance_H1 = 10
-
 
 wavelengths_H1 = file_H1['Wavelenght'].values
 intensities_H1 = file_H1['Intensity'].values
 
+# Hitta toppar
 peaks_H1, props = find_peaks(intensities_H1, prominence=prominence_H1, distance=distance_H1)
 
 H1_wave_peaks = wavelengths_H1[peaks_H1]
 H1_ints_peaks = intensities_H1[peaks_H1]
 
+# Plotta spektra med toppar markerade
 plt.figure()
 plt.plot(wavelengths_H1, intensities_H1)
 plt.scatter(wavelengths_H1[peaks_H1], intensities_H1[peaks_H1], color='red')
@@ -67,6 +75,7 @@ plt.ylabel('Intensitet [arb.]')
 plt.title('Väte-spektra')
 #plt.savefig('H1-spektra.png')
 
+# VÄTE - FIL 2 Identisk med fil 1
 prominence_H2 = 0.008
 distance_H2 = 10
 
@@ -86,6 +95,7 @@ plt.ylabel('Intensitet [arb.]')
 plt.title('H2-spektra')
 #plt.savefig('H2-spektra.png')
 
+# VÄTE - FIL 3 Identisk med fil 1 och 2
 prominence_H3 = 0.008
 distance_H3 = 10
 
@@ -105,23 +115,29 @@ plt.ylabel('Intensitet [arb.]')
 plt.title('H3-spektra')
 #plt.savefig('H3-spektra.png')
 
+
+# Medelvärdera toppar och intensiteter
 all_peaks = np.array([H1_wave_peaks, H2_wave_peaks, H3_wave_peaks])
 all_ints = np.array([H1_ints_peaks, H2_ints_peaks, H3_ints_peaks])
 
 average_peaks = np.mean(all_peaks, axis=0)
 average_ints = np.mean(all_ints, axis=0)
 
+# Hitta standardavvikelse
 std_peaks = np.std(all_peaks, axis=0, ddof=1)
 std_ints = np.std(all_ints, axis=0)
 
+# Ta fram medelfelet av medelvärdet
 sem_peaks = std_peaks / np.sqrt(3)
 
+# Kombinera osäkerheter
 instrument_uncert = 0.05 # step-size
 total_uncert = np.sqrt(sem_peaks**2 + instrument_uncert**2)
 
 wavelengths = np.mean([wavelengths_H1, wavelengths_H2, wavelengths_H3], axis=0)
 intensities = np.mean([intensities_H1, intensities_H2, intensities_H3], axis=0)
 
+# Plotta medelvärderat spektra
 plt.figure()
 plt.plot(wavelengths, intensities)
 plt.scatter(average_peaks, average_ints, color='red')
@@ -136,6 +152,7 @@ peak_ints = np.log10(average_ints + 1e-6)
 peak_ints = peak_ints - peak_ints.min()
 peak_ints = peak_ints / peak_ints.max()
 
+# Matcha våglängd till färg
 def wavelength_to_rgb(wavelength):
     
     if wavelength < 380:  # UV
@@ -160,7 +177,7 @@ def wavelength_to_rgb(wavelength):
 
     return (r, g, b)
 
-# Plot
+# Plotta linjespektra
 plt.figure(figsize=(12, 2))
 plt.xlim(200, 1100)
 plt.ylim(0, 1)
@@ -177,7 +194,7 @@ plt.title("Emissionsspektra")
 
 #plt.savefig('Emissionsspektra-colorfull.png')
 
-
+# Beräkna teoretiska emissionslinjer för väte
 R = 1.097e-2
 
 H_peaks_teo = []
@@ -198,6 +215,7 @@ for n1 in range(1, 4):
 # for i in H_peaks_teo:
 #     print(i['lambda'])
 
+# Kolla om mätta toppar matchar teoretiska linjer inom osäkerhet
 def match_lines(measured, transitions, total_uncert, tol=0.005):
     matches = []
     
